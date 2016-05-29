9a6a4cd7496587dc412520c65229817c0d7251f6
Rob Fonseca-Ensor <robert.ensor@gmail.com>
2016-05-30 10:13:29 +1200

    init

---
+++ Auto/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Auto
+{
+	public static class Extensions
+	{
+		public static void GoToState(this VisualState s, FrameworkElement root, bool useTransitions = true)
+		{
+			VisualStateManager.GoToElementState(root, s.Name, useTransitions);
+		} 
+	}
+}
---
+++ Auto/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DragEventArgs = System.Windows.DragEventArgs;
+using Rectangle = System.Drawing.Rectangle;
+
+namespace Auto
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			In.GoToState(LayoutRoot, false);
+			Left = Screen.AllScreens.Select(screen => screen.Bounds)
+				.Aggregate(Rectangle.Union)
+				.Left;
+		}
+
+		void MainWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+			{
+				this.DragMove();
+			}
+			
+		}
+
+		void MainWindow_OnDragEnter(object sender, DragEventArgs e)
+		{
+			Out.GoToState(LayoutRoot);
+		}
+
+		void MainWindow_OnDragLeave(object sender, DragEventArgs e)
+		{
+			In.GoToState(LayoutRoot);
+		}
+	}
+}
